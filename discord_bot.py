046bde715c82dc65c996e4edc9e2cab47d801aa6
Hoku113 <hokuto.u0113@gmail.com>
2022-06-19 05:32:19 +0900

    discord bot 途中経過

--- discord_bot.py
+++ discord_bot.py
@@ -1,10 +1,15 @@
 import discord
 from discord.ext import tasks
+from discord.channel import TextChannel
 from datetime import datetime
-from discord.ext import commands
 import time
+import random
 
-bot = commands.Bot(command_prefix='$')
+# 本番環境用
+# user_list = []
+test_user_list = []
+level = {'1': 10, '2': 20, '3': 30, '4': 40, '5':50}
+
 
 TOKEN = ""
 CHANNEL_ID = 
@@ -13,17 +18,32 @@ intents = discord.Intents.default()
 
 client = discord.Client()
 
-@client.event
-async def on_ready():
-    print(f'We have logged in as {client.user}')
+@tasks.loop(seconds=1)
+async def loop():
+    now = datetime.now()
+    now = now.strftime('%H:%M')
+    if now == '05:31':
+        channel = TextChannel._get_channel()
+        # user = client.get_user(test_user_list[random.randint(1)])
+        number = level[str(random.randint(1, 5))]
+        await channel.send('hello')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+loop.start()
+
+
+# @client.event
+# async def on_ready():
+#     print(f'We have logged in as {client.user}')
+
+# @client.event
+# async def on_message(message):
+#     if message.author == client.user:
+#         return
     
-    if message.content.startswith('$'):
-        for x in range(10):
-            await message.channel.send("custom stamp")
-            time.sleep(0.25)
+#     if now == '3:56:00':
+#         for x in range(level[str(random.randint(1, 5))]):
+#             await message.channel.send("custom stamp")
+#             time.sleep(2)
+
+    
 client.run(TOKEN)
